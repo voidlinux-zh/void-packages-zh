4b8bfab0f445bee8dbffad3bb537d774c524eace
Nianqing Yao <imbearchild@outlook.com>
2023-11-12 23:33:55 +0800

    add xbps-src

--- extra/update-files-from-void-packages.sh
+++ extra/update-files-from-void-packages.sh
@@ -11,9 +11,11 @@ cd /tmp/void-linux-packages
 OFFICIAL_COMMIT_ID=$(git rev-parse HEAD)
 cp -r common $REPO_PATH
 cp -r etc $REPO_PATH
+cp xbps-src $REPO_PATH
 
 cd $REPO_PATH
 git add ./common/*
 git add ./etc/*
+git add xbps-src
 
 git commit --message "update file from void-packages $OFFICIAL_COMMIT_ID"
